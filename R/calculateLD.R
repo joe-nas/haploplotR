4d3581443c5eb493aaed8a187f077b35cab3a1cc
joe-nas <jonas.falck@student.ru.nl>
2018-02-22 15:00:42 +0100

    changes

--- R/calculateLD.R
+++ R/calculateLD.R
@@ -13,14 +13,16 @@
 
 calculateLD <- function(lead_snps, xSnpMatrix, ...){
   ySnpMatrix <- xSnpMatrix[,which(colnames(xSnpMatrix) %in% lead_snps)] # limit the calculation to this lead_snps
-  snpStats::ld(x = xSnpMatrix, y = ySnpMatrix, stats = c('R.squared'))
+
+  # this function needs optimization! when lead_snps are not found in the data, pairwise LD is calculated on xSnpMatrix x xSnpMatrix
+#  if(dim(ySnpMatrix == xSnpMatrix)){
+#    colnames(xSnpMatrix)
+#  }else{
+
+    snpStats::ld(x = xSnpMatrix, y = ySnpMatrix, stats = c('R.squared'))
+
+#  }
 }
-
-
-
-target_rsquared <- lapply(target_dat, function(dat){
-  snpStats::ld(dat$genotype, dat$genotype, stats = 'R.squared')
-})
 
 my_mapping <- Reduce(rbind,lapply(seq_along(target_dat), function(i){
   prepareMapping(r2 = target_rsquared[[i]],mapping =  target_dat[[i]]$info,r2_cutoff = 0.8,
