26c96259cec6ee2a8b8619c9c5416f2ae495e2f2
joe-nas <jonas.falck@student.ru.nl>
2018-03-05 16:20:15 +0100

    identifyHighLD functions more or less, lead and tag SNPs need to be added to mcols

--- R/identifyHighLD.R
+++ R/identifyHighLD.R
@@ -4,12 +4,22 @@
 
 
 identifyHighLD <- function(rsquared, info, cutoff = 0.8){
+  #identify tag-SNPs
   res <- which(rsquared >= cutoff, arr.ind = T) # rownames: all SNPs in ranges, colnames lead-SNPS
-  res_info <- data.frame(info[res[,1],],rsquared = rsquared[res])
-  res_info
+  res_info_df <- data.frame(info[res[,1],],rsquared = rsquared[res])
+  chromosomes <- Rle(paste("chr",res_info_df$chromosome_num, sep=""))
+  startP <-info[as.numeric(rownames(res_info_df))-1,]$pos
+  endP <- info[as.numeric(rownames(res_info_df))+1,]$pos
+  tag_snps <- GRanges(chromosomes,IRanges(star = startP, end = endP), mcols = res_info_df)
+  tag_snps
 }
 
+constructTagSNPIntervals <-function(res_info){
+  tag_SNP_rowpos <- as.integer(rownames(res_info))
 
+}
+
+  #identifyHighLD(TGData[[3]]$rsquared$CEU, TGData[[3]]$data$CEU$info )
 
 # tagSNPintervals <- function(){
 #
