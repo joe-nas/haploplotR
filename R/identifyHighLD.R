853e26f039451cadf2aa5dcea89cce086490be75
joe-nas <jonas.falck@student.ru.nl>
2018-03-05 17:19:45 +0100

    comments

--- R/identifyHighLD.R
+++ R/identifyHighLD.R
@@ -9,9 +9,10 @@ identifyHighLD <- function(rsquared, info, cutoff = 0.8){
   lead_snps_v <- colnames(rsquared)[res[,2]] # lead_snp vector
   res_info_df <- data.frame(lead_snp = lead_snps_v, info[res[,1],], rsquared = rsquared[res])
   chromosomes <- Rle(paste("chr",res_info_df$chromosome_num, sep=""))
-  tag_snp_r <- as.numeric(rownames(res_info_df))
-  startP <-info[tag_snp_r-1,]$pos
-  endP <- info[tag_snp_r+1,]$pos
+  tag_snp_r <- as.numeric(rownames(res_info_df)) #tag sno rows
+  startP <-info[tag_snp_r-1,]$pos # start position
+  endP <- info[tag_snp_r+1,]$pos # end position
+  # results
   tag_snps <- GRanges(chromosomes, IRanges(star = startP, end = endP),
                       mcols = data.frame(lead_pos = res_info_df$pos, lead_snp = res_info_df$lead_snp,
                                          tag_snp = res_info_df$ids, rs = res_info_df$rsquared))
